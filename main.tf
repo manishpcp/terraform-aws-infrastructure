9caa11db83842d0e74d0f115ff97716ca3bfc1bc
Manish Kumar <manish@iwisebusiness.com>
2025-11-28 17:14:40 +0530

    added

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "my-app-terraform-state-project2"
     key            = "wordpress-ecs/terraform.tfstate"
@@ -6,6 +16,7 @@
     dynamodb_table = "terraform-state-lock2"
   }
 }
+
 
 # VPC Module
 module "vpc" {
